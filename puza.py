f57236ff34b768f94257a8a20508d09d92f5dae6
teknogeek <jmargolis97@gmail.com>
2014-08-23 17:35:44 -0400

    add balances to the end of the donation addresses

--- puza.py
+++ puza.py
@@ -4,6 +4,7 @@ import time
 import os
 from random import randrange
 import json
+import urllib2
 
 
 nick = "DrPuzaBot"
@@ -19,7 +20,7 @@ irc.send('NICK '+nick+'\r\n')
 irc.send('USER PUZA PUZA PUZA :Puza is #1\r\n')
 time.sleep(2)
 irc.send('JOIN '+ras+'\r\n')
-#irc.send('PRIVMSG nickserv :IDENTIFY DrPuza '+nickservpass+'\r\n')
+#irc.send('PRIVMSG nickserv :IDENTIFY DrPuzaBot '+nickservpass+'\r\n')
 while True:
   data = irc.recv(8192)
   print (data)
@@ -32,13 +33,19 @@ while True:
     irc.send('PRIVMSG '+chan+' :\0036► ► ► ► ► \0034LOVE ZONE\0036 ◄ ◄ ◄ ◄ ◄\r\n')
   if firstmsg == "!charity" or firstmsg == "!donate":
     chan = data.split(' ')[2]
+    btcurl = urllib2.urlopen('http://bkchain.org/btc/api/v1/address/balance/1A3k78jypUBPdBxroSBbGdhctNPttDo6FT')
+    dogeurl = urllib2.urlopen('http://bkchain.org/doge/api/v1/address/balance/DN1iT4njrHsb61FowPoHa2BQ49SozHw9ep')
+    ltcurl = urllib2.urlopen('http://bkchain.org/ltc/api/v1/address/balance/LMWYxZ2jbKhkYqU1jWo2LPUY3Wy255o2nu')
+    btcjson = json.load(btcurl)
+    dogejson = json.load(dogeurl)
+    ltcjson = json.load(ltcurl)
     irc.send('PRIVMSG '+chan+' :Donations to the following addresses will all go to charity! =D\r\n')
     time.sleep(0.1)
-    irc.send('PRIVMSG '+chan+' :Every month there will be a different charity recieving the donations.\r\n')
+    irc.send('PRIVMSG '+chan+' :Every month there will be a different charity receiving the donations.\r\n')
     time.sleep(0.1)
-    irc.send('PRIVMSG '+chan+' :BTC:  1A3k78jypUBPdBxroSBbGdhctNPttDo6FT\r\n')
+    irc.send('PRIVMSG '+chan+' :BTC:  1A3k78jypUBPdBxroSBbGdhctNPttDo6FT (Current Balance: '+str(btcjson[0]["balance"]/100000000.0)+' BTC)\r\n')
     time.sleep(0.1)
-    irc.send('PRIVMSG '+chan+' :DOGE: DN1iT4njrHsb61FowPoHa2BQ49SozHw9ep\r\n')
+    irc.send('PRIVMSG '+chan+' :DOGE: DN1iT4njrHsb61FowPoHa2BQ49SozHw9ep (Current Balance: '+str(dogejson[0]["balance"]/100000000.0)+' DOGE)\r\n')
     time.sleep(0.1)
-    irc.send('PRIVMSG '+chan+' :LTC:  LMWYxZ2jbKhkYqU1jWo2LPUY3Wy255o2nu\r\n')
+    irc.send('PRIVMSG '+chan+' :LTC:  LMWYxZ2jbKhkYqU1jWo2LPUY3Wy255o2nu (Current Balance: '+str(ltcjson[0]["balance"]/100000000.0)+' LTC)\r\n')
   time.sleep(0.01)
