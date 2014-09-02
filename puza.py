74300cf17b68c1bcc2e3767931993e1153d9d568
Swanmark <hilmar@gulur.net>
2014-09-02 23:16:09 +0000

    Whatevz

--- puza.py
+++ puza.py
@@ -4,12 +4,12 @@ import time
 import os
 from random import randrange
 import json
-
-
-nick = "PuzaBot"
+import urllib2
+nick = "DrPuzaBot"
 server = "irc.freenode.net"
 ras = "#coinking"
-nickservpass = "INSERT_PASSWORD_HERE"
+ras2 = "##DrPuza"
+nickservpass = "puzalove"
 bSize = "8192"
 irc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 irc.connect((server, 6667))
@@ -19,7 +19,8 @@ irc.send('NICK '+nick+'\r\n')
 irc.send('USER PUZA PUZA PUZA :Puza is #1\r\n')
 time.sleep(2)
 irc.send('JOIN '+ras+'\r\n')
-#irc.send('PRIVMSG nickserv :IDENTIFY DrPuza '+nickservpass+'\r\n')
+irc.send('JOIN '+ras2+'\r\n')
+irc.send('PRIVMSG nickserv :IDENTIFY DrPuzaBot '+nickservpass+'\r\n')
 while True:
   data = irc.recv(8192)
   print (data)
@@ -28,17 +29,30 @@ while True:
   usrmsg = data.split(':')[-1].strip()
   firstmsg = usrmsg.split(' ')[0].lower()
   if firstmsg == "!love":
+    try:
+      arg1 = usrmsg.split(' ')[1]
+    except:
+      arg1 = "error"
     chan = data.split(' ')[2]
-    irc.send('PRIVMSG '+chan+' :\0036► ► ► ► ► \0034LOVE ZONE\0036 ◄ ◄ ◄ ◄ ◄\r\n')
+    if arg1 == "error":
+      irc.send('PRIVMSG '+chan+' :\0036► ► ► ► ► \0034THIS IS THE LOVE ZONE\0036 ◄ ◄ ◄ ◄ ◄\r\n')
+    else:
+      irc.send('PRIVMSG '+chan+' :\0036► ► ► ► ► \0034I LOVE '+str(arg1.upper())+'\0036 ◄ ◄ ◄ ◄ ◄\r\n')
   if firstmsg == "!charity" or firstmsg == "!donate":
     chan = data.split(' ')[2]
+    btcurl = urllib2.urlopen('http://bkchain.org/btc/api/v1/address/balance/1A3k78jypUBPdBxroSBbGdhctNPttDo6FT')
+    dogeurl = urllib2.urlopen('http://bkchain.org/doge/api/v1/address/balance/DN1iT4njrHsb61FowPoHa2BQ49SozHw9ep')
+    ltcurl = urllib2.urlopen('http://bkchain.org/ltc/api/v1/address/balance/LMWYxZ2jbKhkYqU1jWo2LPUY3Wy255o2nu')
+    btcjson = json.load(btcurl)
+    dogejson = json.load(dogeurl)
+    ltcjson = json.load(ltcurl)
     irc.send('PRIVMSG '+chan+' :Donations to the following addresses will all go to charity! =D\r\n')
     time.sleep(0.1)
-    irc.send('PRIVMSG '+chan+' :Every month there will be a different charity recieving the donations.\r\n')
+    irc.send('PRIVMSG '+chan+' :Every month there will be a different charity receiving the donations.\r\n')
     time.sleep(0.1)
-    irc.send('PRIVMSG '+chan+' :BTC:  1A3k78jypUBPdBxroSBbGdhctNPttDo6FT\r\n')
+    irc.send('PRIVMSG '+chan+' :BTC: 1A3k78jypUBPdBxroSBbGdhctNPttDo6FT (Current Balance: '+str(btcjson[0]["balance"]/100000000.0)+' BTC)\r\n')
     time.sleep(0.1)
-    irc.send('PRIVMSG '+chan+' :DOGE: DN1iT4njrHsb61FowPoHa2BQ49SozHw9ep\r\n')
+    irc.send('PRIVMSG '+chan+' :DOGE: DN1iT4njrHsb61FowPoHa2BQ49SozHw9ep (Current Balance: '+str(dogejson[0]["balance"]/100000000.0)+' DOGE)\r\n')
     time.sleep(0.1)
-    irc.send('PRIVMSG '+chan+' :LTC:  LMWYxZ2jbKhkYqU1jWo2LPUY3Wy255o2nu\r\n')
-  time.sleep(0.01)
+    irc.send('PRIVMSG '+chan+' :LTC: LMWYxZ2jbKhkYqU1jWo2LPUY3Wy255o2nu (Current Balance: '+str(ltcjson[0]["balance"]/100000000.0)+' LTC)\r\n')
+    time.sleep(0.01)
